91e2526cf939669f81f247bbaec7c807d0eabcb0
ngducnhat <ngducnhat@gmail.com>
2015-03-24 01:34:57 +0700

    Update MainMenuScreen

--- Assets/Scripts/Game/Screens/MainMenuScreen.cs
+++ Assets/Scripts/Game/Screens/MainMenuScreen.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class MainMenuScreen : BaseScreen {
 
+	public UIPanel panel;
+
 	public override void Init(params object[] inputs) {
 		base.Init(inputs);
+		panel = (Instantiate(Resources.Load(prefabPath + "MainMenuUI")) as GameObject).GetComponent<UIPanel>();
+		panel.transform.parent = thisTransform;
 	}
 }
